c250315a5aac2ab8f9e64f1bcf042afbc64cb26e
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2021-02-24 19:25:44 +1000

    built calendar-picker app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
+export default function App() {
+  const calendarDates = Array(31).fill(0).map((e, i) => i);
+
+  const [choosingDate, setChoosingDate] = useState('start');
+  const [hoverEffect, setHoverEffect] = useState(null);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
+
+  const handleUpdateCalendar = (day) => {
+    if (startDate && day < startDate) {
+      setStartDate(day);
+      return setChoosingDate('end');
+    }
+    if (endDate && day > endDate) {
+      setEndDate(day);
+      return setChoosingDate('end');
+    }
+    if (choosingDate === 'start') {
+      setStartDate(day);
+      return setChoosingDate('end');
+    }
+    if (choosingDate === 'end') {
+      setEndDate(day);
+    }
+  }
+
+  const handleIsBetween = (day) => {
+    if (startDate && !endDate) return day > startDate && day < hoverEffect;
+    return day > startDate && day < endDate;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="date-chooser">
+        <button
+          className={`date-chooser-button ${choosingDate === 'start' ? 'ischoosing' : ''}`}
+          onClick={() => setChoosingDate('start')}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Start Date <span>{startDate}</span>
+        </button>
+        <button
+          className={`date-chooser-button ${choosingDate === 'end' ? 'ischoosing' : ''}`}
+          onClick={() => setChoosingDate('end')}
+        >
+          End Date <span>{endDate}</span>
+        </button>
+      </div>
+      <div className="calendar">
+        {calendarDates.map((day, index) => {
+
+          const dayNumber = day + 1;
+          let selected = dayNumber === startDate || dayNumber === endDate;
+          let isBetween = handleIsBetween(dayNumber);
+
+          return (
+            <button
+              className={`calendar-day ${selected ? 'selected' : ''} ${isBetween ? 'between' : ''}`}
+              key={index}
+              onClick={() => handleUpdateCalendar(dayNumber)}
+              onMouseOver={() => setHoverEffect(dayNumber)}
+            >
+              {dayNumber}
+            </button>)
+        })}
+      </div>
+    </>
   );
 }
-
-export default App;
